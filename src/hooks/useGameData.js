fb942bf88981c7629a5ce13392fcdaf8e580a5e6
Emils Melders <emkons3@gmail.com>
2022-07-20 00:50:05 +0300

    Fix 0th index song

--- src/hooks/useGameData.js
+++ src/hooks/useGameData.js
@@ -9,7 +9,7 @@ const useGameData = (playlistId, date) => {
     const finished = useMemo(() => playlistData?.finished || false, [playlistData])
     const correct = useMemo(() => playlistData?.correct || false, [playlistData])
     const todayTrack = useMemo(() => playlistData?.todayTrack || null, [playlistData])
-    const todayTrackIndex = useMemo(() => playlistData?.todayTrackIndex || null, [playlistData])
+    const todayTrackIndex = useMemo(() => playlistData?.todayTrackIndex === undefined ? null : playlistData?.todayTrackIndex, [playlistData])
     const loading = playlistData === 'loading'
 
     const updateValue = async (newValue) => {
