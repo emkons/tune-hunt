bb67a5848fcfe88b74113586639a999f06fd1503
Emils Melders <emkons3@gmail.com>
2022-08-22 23:29:50 +0300

    Revert playlist data saving

--- src/db.js
+++ src/db.js
@@ -2,8 +2,9 @@ import Dexie from 'dexie';
 
 export const db = new Dexie('myDatabase');
 
-db.version(8).stores({
+db.version(9).stores({
     playlistHistory: '[id+date], id, date', // Primary key and indexed props
+    playlist: 'id',
     settings: 'key',
     favourites: 'id, position',
     tracks: '[key+playlist], playlist, key',
